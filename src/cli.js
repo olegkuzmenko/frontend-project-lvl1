27bbb161b0d948fb98da17f0f9b4ef7b7346e480
oleg.v.kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-05-15 11:38:59 +0300

    step 3.0: adding readline sync lib

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export const askUserName = () => {
+    const userName = readlineSync.question('May I have your name?');
+    console.log(`Hello, ${userName}!`);
+};
