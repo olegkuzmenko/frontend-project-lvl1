7de2b3ed98d91bf16e9821c4d9cea50fb7f9d624
oleg.v.kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-05-15 16:21:41 +0300

    step 4.0: adding ESlint, CodeClimate, GitHub Actions

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
-export const askUserName = () => {
-    const userName = readlineSync.question('May I have your name?');
+
+export default () => {
+    const userName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${userName}!`);
 };
