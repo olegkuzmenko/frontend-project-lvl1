bed1aaddc3729bba6c336cb0eb7ad458413fe589
oleg.v.kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-05-18 12:52:06 +0300

    5.0: adding brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { askUserName, roundGame } from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = askUserName();
+
+const counter = 3;
+
+roundGame(counter, userName);
+
+
+
+
+
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,63 @@
 import readlineSync from 'readline-sync';
 
-const askUserName = () => {
+export const askUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
-export default askUserName;
+export const getAnswer = (integer) => {
+  const answer = readlineSync.question(`Question: ${integer}\nYour answer: `);
+  return answer;
+};
+
+export const wrongAnswerReport = (str, name) => {
+  const currentAnswer = str;
+  const oppositeAnswer = '';
+  const userName = name;
+  if (currentAnswer === 'yes') {
+    oppositeAnswer = 'no';
+  } else {
+    oppositeAnswer = 'yes';
+  }
+  console.log(`"${currentAnswer}" is wrong answer ;(. Correct answer was "${oppositeAnswer}".
+  Let's try again, ${userName}`)
+};
+
+
+export const checkAnswer = (str, boolean) => {
+  const booleanAnswer = true;
+  if (str === 'no') {
+    booleanAnswer = false;
+  }
+  return booleanAnswer === boolean ? true : false;
+};
+
+export const getRandomInteger = () => {
+  const min = 1;
+  const max = 100;
+  const result = Math.floor(Math.random() * (max - min)) + min;
+  return result;
+};
+
+export const isEven = (integer) => integer % 2 !== 0 ? false : true; 
+
+export const roundGame = (counter, userName) => {
+  if (counter > 0) {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    const currentNumber = getRandomInteger();
+
+    const answer = getAnswer(currentNumber);
+
+    const rightAnswer = isEven(currentNumber)
+
+    const statusOfAnswer = checkAnswer(answer, rightAnswer);
+
+    if (statusOfAnswer === true) {
+      console.log('Correct!');
+      return roundGame(counter - 1);
+    }
+    return wrongAnswerReport(answer, userName);
+  }
+};
