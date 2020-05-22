f0415bcedf9cbfe763ce5d260e7fb1d1d7233c4e
oleg.v.kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-05-22 15:28:17 +0300

    bugFix bin/brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,4 +6,4 @@ import gameBrainEven from '../src/games/game-brain-even.js';
 const gameName = 'even';
 const howManyRounds = 3;
 
-gameEngine(gameBrainEven, howManyRounds, gameName);
+gameEngine(gameBrainEven(), howManyRounds, gameName);
